f168f64b6c52b36b1e2b4e4b5fc80e55c3dd7b90
joeleung <joeleung@hket.com>
2023-12-07 17:31:52 +0800

    add custom logger function

--- logger/src/lib.rs
+++ logger/src/lib.rs
@@ -1,6 +1,7 @@
-static mut LOGGER_ENVIRONMENT: LoggerEnv = LoggerEnv::DEV;
+static mut LOGGER_LEVEL: LoggerLevel = LoggerLevel::INFO;
 
 /// logger level
+#[derive(PartialEq, PartialOrd, Debug)]
 pub enum LoggerLevel {
     DEBUG,
     INFO,
@@ -8,54 +9,60 @@ pub enum LoggerLevel {
     ERROR,
 }
 
-pub fn log_macro(level: LoggerLevel, message: String) {
-    println!(
-        "{{ \"level\": \"{}\", \"message\": \"{}\" }}",
-        unsafe {
-            match level {
-                LoggerLevel::DEBUG if LOGGER_ENVIRONMENT != LoggerEnv::DEBUG => return,
-                LoggerLevel::DEBUG => "debug",
-                LoggerLevel::INFO => "info",
-                LoggerLevel::WARN => "warn",
-                LoggerLevel::ERROR => "error",
-            }
-        },
-        message,
-    );
-}
-
-/// logger environment
-#[derive(Debug, PartialEq)]
-pub enum LoggerEnv {
-    DEBUG,
-    DEV,
-    UAT,
-    PROD,
-}
-
-pub fn set_logger_env(env: LoggerEnv) {
-    unsafe {
-        LOGGER_ENVIRONMENT = env;
+impl std::fmt::Display for LoggerLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "logger::LoggerLevel::{:?}", self)
     }
 }
 
-pub fn get_logger_env() -> &'static LoggerEnv {
-    unsafe { &LOGGER_ENVIRONMENT }
+pub fn log_macro(level: LoggerLevel, message: String, extra_fields: Vec<(&str, &str)>) {
+    let can_print: bool = &level >= unsafe { &LOGGER_LEVEL };
+    if can_print {
+        let mut print_stack: Vec<String> = vec![
+            format!(
+                "\"level\": \"{}\"",
+                match level {
+                    LoggerLevel::DEBUG => "debug",
+                    LoggerLevel::INFO => "info",
+                    LoggerLevel::WARN => "warn",
+                    LoggerLevel::ERROR => "error",
+                }
+            ),
+            format!("\"message\": \"{}\"", message),
+        ];
+        extra_fields.iter().for_each(|&(key, value)| {
+            print_stack.push(format!("\"{key}\": \"{value}\""));
+        });
+        println!("{{ {} }}", print_stack.join(", "));
+    }
+}
+
+pub fn set_logger_level(level: LoggerLevel) {
+    unsafe {
+        LOGGER_LEVEL = level;
+    }
+}
+
+pub fn get_logger_level() -> &'static LoggerLevel {
+    unsafe { &LOGGER_LEVEL }
 }
 
 /// custom logger log function
 #[macro_export]
 macro_rules! log {
 	( debug $( $arg:tt ), * ) => {{
-		logger::log_macro(logger::LoggerLevel::DEBUG, format!($($arg),*));
+		logger::log_macro(logger::LoggerLevel::DEBUG, format!($($arg),*), vec![]);
 	}};
 	( info $( $arg:tt ), * ) => {{
-		logger::log_macro(logger::LoggerLevel::INFO, format!($($arg),*));
+		logger::log_macro(logger::LoggerLevel::INFO, format!($($arg),*), vec![]);
 	}};
 	( warn $( $arg:tt ), * ) => {{
-		logger::log_macro(logger::LoggerLevel::WARN, format!($($arg),*));
+		logger::log_macro(logger::LoggerLevel::WARN, format!($($arg),*), vec![]);
 	}};
 	( error $( $arg:tt ), * ) => {{
-		logger::log_macro(logger::LoggerLevel::ERROR, format!($($arg),*));
+		logger::log_macro(logger::LoggerLevel::ERROR, format!($($arg),*), vec![]);
+	}};
+	( $key:ident $v:expr, $( $arg:tt ), * ) => {{
+		logger::log_macro(logger::LoggerLevel::$key, format!($($arg),*), $v);
 	}};
 }
